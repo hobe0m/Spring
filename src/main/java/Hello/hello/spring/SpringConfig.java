87dc2e8c29f3237074d8e3500fbf60f1edd79c7d
hobe0m <beom231231@gmail.com>
2025-02-09 21:07:12 +0900

    [feat] Member Management(Spring JDBC Template)

--- src/main/java/Hello/hello/spring/SpringConfig.java
+++ src/main/java/Hello/hello/spring/SpringConfig.java
@@ -1,6 +1,7 @@
 package Hello.hello.spring;
 
 import Hello.hello.spring.repository.JdbcMemberRepository;
+import Hello.hello.spring.repository.JdbcTemplateMemberRepository;
 import Hello.hello.spring.repository.MemberRepository;
 import Hello.hello.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +35,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
     
     // 참고
---
+++ src/main/java/Hello/hello/spring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,72 @@
+package Hello.hello.spring.repository;
+
+import Hello.hello.spring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository {
+
+    // Spring JDBC Template
+
+    private JdbcTemplate jdbcTemplate;
+
+    // 생성자가 딱 하나일 경우에는 @AutoWired를 생략할 수 있다.
+    // 지금과 같은 경우
+    @Autowired
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        // Insert문 만들기
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
+    }
+
+    // JDBC 코드와 비교하면, Template를 사용했을 때 간결해지는 것을 확인할 수 있다.
+    @Override
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member", memberRowMapper());
+    }
+
+    // lambda로 변경
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+}
