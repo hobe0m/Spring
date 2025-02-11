000c90135f68649379a282c7e6987069368ce4c3
hobe0m <beom231231@gmail.com>
2025-02-11 23:12:37 +0900

    [feat] Member Management(Spring JPA)

--- src/main/java/Hello/hello/spring/domain/Member.java
+++ src/main/java/Hello/hello/spring/domain/Member.java
@@ -1,8 +1,14 @@
 package Hello.hello.spring.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Member {
 
     // 고객이 정하는 것이 아닌, 시스템이 정하는 id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     // 이름
